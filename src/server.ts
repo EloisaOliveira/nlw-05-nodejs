8e7c038bfd829742c8a525cf6bd2c1ff9cd7fa2d
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2021-04-19 14:22:00 -0300

    created the first route

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express()
+
+app.get("/", (req, res) => {
+    return res.json({
+        message: "Olá NLW 05!"
+    })
+})
+
+app.listen(3333, () => console.log("Server is running on port 3333"))

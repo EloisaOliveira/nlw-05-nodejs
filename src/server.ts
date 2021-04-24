7b414243ed4048c6659316742b2f1b2cd7e39f3e
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2021-04-24 20:10:58 -0300

    feat(websocket): configured the websocket

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io"
 
 import "./database"
 import { routes } from "./routes"
 
 const app = express()
+
+const http = createServer(app) //criando protocolo http
+const io = new Server(http) //criandp protocolo websocket
+
+io.on("connection", (socket: Socket) =>{
+    console.log("Se conectou", socket.id);
+});
 
 app.use(express.json());
 
